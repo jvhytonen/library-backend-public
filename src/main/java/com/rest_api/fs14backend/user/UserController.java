f8b92d8f2b1aaea1f5ffc32e94448092efc24cbc
Olga Risteli <olga.a.risteli@gmail.com>
2023-05-03 19:22:26 +0300

    Change db username in configuration Add user class, repository, controllers, and services

---
+++ src/main/java/com/rest_api/fs14backend/user/UserController.java
@@ -0,0 +1,32 @@
+package com.rest_api.fs14backend.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User addOne(@RequestBody User user) {
+        return userService.addOne(user);
+    }
+    @GetMapping("/")
+    public List<User> getAll(){
+        return userService.getAllUsers();
+    }
+    @GetMapping(path="/{id}")
+    public Optional<User> getUserById(@PathVariable("id") UUID id) {
+        return Optional.ofNullable(userService.findById(id));
+    }
+    @DeleteMapping(path="/{id}")
+    public void deleteUserById(@PathVariable("id") UUID id){
+        userService.deleteById(id);
+    }
+}
---
+++ src/main/java/com/rest_api/fs14backend/user/UserService.java
@@ -0,0 +1,28 @@
+package com.rest_api.fs14backend.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepo;
+
+    public User addOne(User user) {
+        return userRepo.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    public User findById(UUID userId) {
+        return userRepo.findById(userId).orElse(null);
+    }
+    public void deleteById(UUID userId) {
+        userRepo.deleteById(userId);
+    }
+}
