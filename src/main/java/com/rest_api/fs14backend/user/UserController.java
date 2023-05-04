b48b0cb3b8db59c1bcc9cc2f7983bed29bec8f4c
Olga Risteli <olga.a.risteli@gmail.com>
2023-05-04 17:40:44 +0300

    Add class, DTO for loans Add function get loans by user id

--- src/main/java/com/rest_api/fs14backend/user/UserController.java
+++ src/main/java/com/rest_api/fs14backend/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -24,6 +25,13 @@ public class UserController {
     @GetMapping(path="/{id}")
     public Optional<User> getUserById(@PathVariable("id") UUID id) {
         return Optional.ofNullable(userService.findById(id));
+    }
+
+    @GetMapping(path="/{id}/loans")
+    public Optional<List<UserLoanDTO>> getLoansByUserId(@PathVariable("id") UUID id) {
+        return Optional.ofNullable(userService.findById(id))
+                .map(User::getLoans)
+                .map(loans -> loans.stream().map(UserLoanDTO::fromLoan).collect(Collectors.toList()));
     }
     @DeleteMapping(path="/{id}")
     public void deleteUserById(@PathVariable("id") UUID id){
