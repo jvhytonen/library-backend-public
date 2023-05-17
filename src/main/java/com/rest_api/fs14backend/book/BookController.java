b1993a1d383151e5999243748216f73bb8d85753
Juho-Veikko Hytönen <jv.hytonen@gmail.com>
2023-05-17 11:10:37 +0300

    Mappers added and some fixes.

--- src/main/java/com/rest_api/fs14backend/book/BookController.java
+++ src/main/java/com/rest_api/fs14backend/book/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@CrossOrigin(origins = "http://127.0.0.1:5173/")
 @RestController
 @RequestMapping("api/v1/books")
 @RequiredArgsConstructor
