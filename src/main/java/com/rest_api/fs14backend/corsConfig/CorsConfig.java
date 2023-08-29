c83fab5d32911731c59df811e398e92337bd8f04
Juho-Veikko Hytönen <jv.hytonen@gmail.com>
2023-08-29 12:37:49 +0300

    Cors config added again.

--- src/main/java/com/rest_api/fs14backend/corsConfig/CorsConfig.java
+++ src/main/java/com/rest_api/fs14backend/corsConfig/CorsConfig.java
@@ -1,22 +1,25 @@
-package com.rest_api.fs14backend.CorsConfig;
+package com.rest_api.fs14backend.corsConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        //Localhost
-        config.addAllowedOrigin("http://127.0.0.1:5173");
-        config.addAllowedOrigin("https://stately-starship-e19365.netlify.app");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://127.0.0.1:5173/");
+        configuration.addAllowedOrigin("https://fs14-backend-6whg.onrender.com/api/");
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        //This will apply to all Controllers
-        source.registerCorsConfiguration("/**", config);
+        //Books and authors
+        source.registerCorsConfiguration("/api/books/**", configuration);
+        source.registerCorsConfiguration("/api/authors/**", configuration);
 
         return source;
     }
